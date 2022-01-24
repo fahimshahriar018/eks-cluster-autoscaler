fe07119c7acd68cfd4dd4250f91b240d210c6343
Fahim <fahim@workstation.fahim.com>
2022-01-24 21:32:15 +0600

    1st commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Configuring EKS Cluster Auto-scaler using AWS cli and Helm
+
+
+## Cluster Autoscaler
+The Kubernetes Cluster Autoscaler automatically adjusts the number of nodes in your cluster when pods fail or are rescheduled onto other nodes. The Cluster Autoscaler is typically installed as a Deployment in your cluster. It uses leader election to ensure high availability, but scaling is done by only one replica at a time.
+
+##  Prerequisites
+Before deploying the Cluster Autoscaler, you must meet the following prerequisites:
+
+1. An existing Amazon EKS cluster – If you don’t have a cluster, see Creating an Amazon EKS cluster.
+
+2. An existing IAM OIDC provider for your cluster. To determine whether you have one or need to create one, see Create an IAM OIDC provider for your cluster.
+
+3. Node groups with Auto Scaling groups tags. The Cluster Autoscaler requires the following tags on your Auto Scaling groups so that they can be auto-discovered.
+
+    |Key                                       | value  |
+    |----------------------------------------  |:------:|
+    |k8s.io/cluster-autoscaler/**cluster-name**| owned  |
+    | k8s.io/cluster-autoscaler/enabled        |  TRUE  |
+
+4. [AWS Cli v2](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html) and [Helm](https://helm.sh/) must be installed in your local linux machine. 
+
+## Steps:
+1. download this repository.
+
+    ``` 
+        git clone test 
+    ```
+2. Go to ***script*** folder and edit ***script.sh***
+    ``` 
+        cd script/ 
+        vim script.sh
+    ```
+
+    Change the variables value accordingly. Variables are described below:
+    
+   + ***env*** = Your cluster environment value. (Example: dev/stage/prod)
+   + ***iamPolicyName*** = Name of the iam policy.
+   + ***iamRole*** = iam Role name required for service account
+   + ***accountId*** = Your AWS Account ID.
+   + ***region*** = Your EKS Cluster Region
+   + ***clusterName*** = Your cluster name
+   + ***clusterAutoScalerImageTag*** = Cluster Autoscaler image tag(Example v1.21.2)  Open the Cluster Autoscaler [releases](https://github.com/kubernetes/autoscaler/releases) page from GitHub in a web browser and find the latest Cluster Autoscaler version that matches the Kubernetes major and minor version of your cluster. For example, if the Kubernetes version of your cluster is 1.20, find the latest Cluster Autoscaler release that begins with 1.20. Ue  the semantic version number (1.20.n) for that release in value.
+3. add execute permission to run the script and run it. 
+    ```
+    chmod +x script.sh
+    . script.sh
+    ```
+    Deployment is done. 
+## View your Cluster Autoscaler logs
+
+After you have deployed the Cluster Autoscaler, you can view the logs and verify that it's monitoring your cluster load.
+
+View your Cluster Autoscaler logs with the following command.
+
+```
+kubectl -n kube-system logs -f deployment.apps/cluster-autoscaler
+```
+## Reference:
++ https://docs.aws.amazon.com/eks/latest/userguide/autoscaling.html#ca-deployment-considerations
+
---
+++ script/script.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+env="test"
+iamPolicyName="${env}EKSClusterAutoScalerPolicy"
+iamRole="${env}EKSClusterAutoScalerRole"
+accountId="12345678"
+region="us-west-2"
+clusterName="test-cluster"
+clusterAutoScalerImageTag="v1.20.2"
+clusterOIDCProvider=$(aws eks describe-cluster --name ${clusterName} --region ${region} --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+
+redColor='\e[1;31m'
+greenColor='\e[1;32m'
+noColor='\e[0m'
+
+# Create iam policy
+cat <<EoF > ./cluster-autoscaler-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+}
+EoF
+
+
+aws iam create-policy --policy-name=${iamPolicyName}  --policy-document file://./cluster-autoscaler-policy.json
+if [ $? -ne 0 ]
+then
+  echo -e "${redColor} Policy isn't created ${noColor}"
+else
+  echo -e "${greenColor} Policy:${iamPolicyName} is  created ${noColor} "
+fi
+
+sleep 5
+
+
+# Create trust policy
+cat <<EoF > ./iam-role-trust-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Federated": "arn:aws:iam::${accountId}:oidc-provider/${clusterOIDCProvider}"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+          "StringEquals": {
+            "${clusterOIDCProvider}:sub": "system:serviceaccount:kube-system:cluster-autoscaler"
+          }
+        }
+      }
+    ]
+}
+EoF
+
+# Create iam role for service account
+aws iam create-role --role-name ${iamRole} --assume-role-policy-document file://./iam-role-trust-policy.json
+if [ $? -ne 0 ]
+then
+  echo -e "${redColor} iam Role  isn't created ${noColor}"
+else
+  echo -e "${greenColor} iam Role:${iamRole} is  created ${noColor} "
+fi
+
+sleep 5
+
+# Attach policy to the role
+aws iam attach-role-policy --role-name ${iamRole} --policy-arn "arn:aws:iam::${accountId}:policy/${iamPolicyName}"
+if [ $? -ne 0 ]
+then
+  echo -e "${redColor} policy isn't attached to iam Role ${noColor}"
+else
+  echo -e "${greenColor} policy is attached to iam Role ${noColor} "
+fi
+
+sleep 5
+
+# Deploy Cluster auto-sclaer component in the EKS cluster
+cat <<EoF > ../cluster-autoscaler/${env}-values.yaml
+serviceAccountRoleARN: arn:aws:iam::${accountId}:role/${iamRole}
+clusterName: ${clusterName}
+image:
+  tag: ${clusterAutoScalerImageTag}
+EoF
+
+cd ../
+helm -n kube-system install --values=cluster-autoscaler/${env}-values.yaml cluster-autoscaler cluster-autoscaler
+if [ $? -ne 0 ]
+then
+  echo -e "${redColor} Cluster-autoscaler components deployment in ${clusterName} Went Wrong!!! ${noColor}"
+else
+  echo -e "${greenColor} Cluster-autoscaler components deployment in ${clusterName} is Successful. ${noColor} "
+
+fi
